2ac02edcb74bf8fe296655eb2f0c00bbbf26f562
Artem Lipatkin <lipatkin96@gmail.com>
2017-09-22 22:41:29 +0700

    Change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
+
+//#define TEST_MODE
+
+#ifdef TEST_MODE
 #include <gtest/gtest.h>
+#else
+
+#endif
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+#ifdef TEST_MODE
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
+#else
+
+
+
+	return 0;
+#endif
+
 }
 
